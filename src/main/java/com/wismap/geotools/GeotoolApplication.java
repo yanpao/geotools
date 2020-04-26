a78f756b4e01fa2d3c82a78a41c567bec0ff27a2
yanpao <lihanzhang@outlook.com>
2020-04-27 00:30:17 +0800

    切面，并上传到数据库

--- src/main/java/com/wismap/geotools/GeotoolApplication.java
+++ src/main/java/com/wismap/geotools/GeotoolApplication.java
@@ -1,12 +1,21 @@
 package com.wismap.geotools;
 
-import org.geotools.data.DataStore;
-import org.geotools.data.DataStoreFinder;
+import org.geotools.data.*;
+import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
-import org.locationtech.jts.geom.MultiPolygon;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureImpl;
+import org.geotools.process.geometry.GeometryFunctions;
+import org.geotools.process.vector.ClipProcess;
+import org.locationtech.jts.geom.*;
+import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -24,8 +33,8 @@ public class GeotoolApplication {
         params.put("host", "localhost");
         params.put("port", 5432);
         params.put("schema", "public");
-        params.put("database", "postgis");
-        params.put("user", "postgis");
+        params.put("database", "mygis");
+        params.put("user", "mygis");
         params.put("passwd", "wismap123");
 
         try {
@@ -39,17 +48,60 @@ public class GeotoolApplication {
                 featureList.add(feature);
             }
             itertor.close();
-            //计算得到多边形面积
+
             SimpleFeature sf = featureList.get(0);
             MultiPolygon geom=(MultiPolygon)sf.getDefaultGeometry();
-            System.out.println(geom.getArea());
+            Geometry polygon=geom.getGeometryN(0);
+
+            SimpleFeatureSource featureSource2 =dataStore.getFeatureSource("clipline");
+            SimpleFeatureCollection result2 = featureSource2.getFeatures();
+            FeatureIterator<SimpleFeature> itertor2 = result2.features();
+            DefaultFeatureCollection featureList2 = new DefaultFeatureCollection();
+            SimpleFeature featuressss=itertor2.next();
+            MultiLineString line = (MultiLineString)featuressss.getDefaultGeometry();
+
+            Geometry resultss = GeometryFunctions.splitPolygon(polygon,(LineString) line.getGeometryN(0));
+            FeatureStore featureStore=(FeatureStore)dataStore.getFeatureSource("COUA3");
+
+
+            Transaction session = new DefaultTransaction("Adding");
+            featureStore.setTransaction( session );
+            DefaultFeatureCollection result2222=new DefaultFeatureCollection();
+
+            GeometryCollection geometryCollection=((GeometryCollection)resultss);
+            for (int i=0;i<geometryCollection.getNumGeometries();i++)
+            {
+                SimpleFeatureType TYPE = dataStore.getFeatureSource("COUA3").getSchema();
+                SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(TYPE);
+                featureBuilder.add(geometryCollection.getGeometryN(i));
+                SimpleFeature feature = featureBuilder.buildFeature(null);
+                result2222.add(feature);
+
+            }
+
+            featureStore.addFeatures(result2222);
+            session.commit();
         }
         catch (Exception ex)
         {
-
+            System.out.print(ex.getMessage());
         }
 
         SpringApplication.run(GeotoolApplication.class, args);
+    }
+
+    public static SimpleFeatureCollection getLines(DataStore dataStore) throws Exception
+    {
+        SimpleFeatureSource featureSource =dataStore.getFeatureSource("LRRL");
+        SimpleFeatureCollection result = featureSource.getFeatures();
+        FeatureIterator<SimpleFeature> itertor = result.features();
+        DefaultFeatureCollection featureList = new DefaultFeatureCollection();
+        while (itertor.hasNext()) {
+            SimpleFeature feature = itertor.next();
+            featureList.add(feature);
+        }
+        itertor.close();
+        return featureList;
     }
 
 }
